13ed9a2f1b739c1edc2791bf15722542633170c0
AlejandroLucas161 <alejandrolucas161@gmail.com>
2022-04-29 23:20:53 -0300

    test

--- e-commerce-alejandro-lucas/src/components/Item/Item.js
+++ e-commerce-alejandro-lucas/src/components/Item/Item.js
@@ -7,10 +7,10 @@ const Item = ({ name, price, imageUrl, stock }) => {
         <img src={imageUrl} alt={name} className='item__image' />
       </div>
 
-      <h2 className='item__name'>{name}</h2>
+      <h2 className='item__name'>ROMPIENDO TODOOOOOOOOOOOOOOOOOOOO</h2>
 
       <div className='item__info'>
-        <span className='item__info-price'>Precio: <b>${price}</b></span>
+        <span className='item__info-price'>GUITA TOTAL A PAGAR: <b>${price}</b></span>
         <span className='item__info-stock'>Stock: <b>{stock}</b></span>
       </div>
     </Fragment>
